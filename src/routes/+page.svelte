adc476e242289ad6cb733e83dde2c64a456ebec0
Chad Tennent <stealthbos@protonmail.com>
2023-03-03 13:26:30 -0800

    It works!

--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,16 +1,24 @@
 <script>
 	import { Styles } from 'sveltestrap';
-	import { Container, Col, Button, Row } from 'sveltestrap';
+	import { evaluate } from 'mathjs';
+	//import { Container, Col, Button, Row } from 'sveltestrap';
 	let display = '0';
 
-	const addDigit = () => {
-		console.log(this.innerHTML);
+	const addDigit = (event) => {
+		if (display === '0') {
+			display = '';
+		}
+		display += event.target.value;
 	};
 
 	const getResult = () => {
-		console.log(display);
-		//eval(display)
+		let result = evaluate(document.getElementById('result').value);
+		display = result;
 	};
+
+	//TODO: Refactor. Display compononet + numbers component? Svelte might also be able to generate the buttons
+	//		with a loop so it's not so boilerplate-y.
+	//TODO: Squeeze in a backspace button somewhere.
 </script>
 
 <div>
@@ -19,61 +27,69 @@
 			<div class="col-auto">
 				<input class="display" type="text" id="result" value={display} disabled />
 			</div>
-			<div class="col"><div class="btn btn-secondary">C</div></div>
-			<div class="col"><div class="btn btn-secondary">/</div></div>
+			<div class="col"><div class="btn btn-secondary" on:click={() => (display = '0')}>C</div></div>
+			<div class="col">
+				<button class="btn btn-secondary" on:click={(e) => addDigit(e)} value="/">/</button>
+			</div>
 		</div>
+		<!--display component-->
 		<div class="row justify-content-around">
 			<div class="col-1">
-				<button type="button" class="btn btn-secondary" on:click={() => addDigit()} value="1"
+				<button type="button" class="btn btn-secondary" on:click={(e) => addDigit(e)} value="1"
 					>1</button
 				>
 			</div>
 			<div class="col-1">
-				<button class="btn btn-secondary" on:click={() => addDigit()}>2</button>
+				<button class="btn btn-secondary" on:click={(e) => addDigit(e)} value="2">2</button>
 			</div>
 			<div class="col-1">
-				<button class="btn btn-secondary" on:click={() => console.log(this.innerHTML)}>3</button>
+				<button class="btn btn-secondary" on:click={(e) => addDigit(e)} value="3">3</button>
 			</div>
 			<div class="col-1">
-				<button class="btn btn-secondary" on:click={() => addDigit()}>4</button>
+				<button class="btn btn-secondary" on:click={(e) => addDigit(e)} value="4">4 </button>
 			</div>
 			<div class="col-2">
-				<button class="btn btn-secondary" on:click={() => addDigit()}>+</button>
+				<button class="btn btn-secondary" on:click={(e) => addDigit(e)} value="+">+</button>
 			</div>
 		</div>
+		<!--NumberRow component-->
 		<div class="row justify-content-around">
 			<div class="col-1">
-				<button type="button" class="btn btn-secondary" on:click={() => addDigit()}>5</button>
+				<button type="button" class="btn btn-secondary" on:click={(e) => addDigit(e)} value="5"
+					>5</button
+				>
 			</div>
 			<div class="col-1">
-				<button class="btn btn-secondary" on:click={() => addDigit()}>6</button>
+				<button class="btn btn-secondary" on:click={(e) => addDigit(e)} value="6">6</button>
 			</div>
 			<div class="col-1">
-				<button class="btn btn-secondary" on:click={() => addDigit()}>7</button>
+				<button class="btn btn-secondary" on:click={(e) => addDigit(e)} value="7">7</button>
 			</div>
 
 			<div class="col-1">
-				<button class="btn btn-secondary" on:click={() => addDigit()}>8</button>
+				<button class="btn btn-secondary" on:click={(e) => addDigit(e)} value="8">8</button>
 			</div>
 			<div class="col-2">
-				<button class="btn btn-secondary" on:click={() => addDigit()}>-</button>
+				<button class="btn btn-secondary" on:click={(e) => addDigit(e)} value="-">-</button>
 			</div>
 		</div>
 		<div class="row justify-content-around">
 			<div class="col-1">
-				<button type="button" class="btn btn-secondary" on:click={() => addDigit()}>9</button>
+				<button type="button" class="btn btn-secondary" on:click={(e) => addDigit(e)} value="9"
+					>9</button
+				>
 			</div>
 			<div class="col-1">
-				<button class="btn btn-secondary" on:click={() => addDigit()}>0</button>
+				<button class="btn btn-secondary" on:click={(e) => addDigit(e)} value="0">0</button>
 			</div>
 			<div class="col-1">
-				<button class="btn btn-secondary" on:click={() => addDigit()}>.</button>
+				<button class="btn btn-secondary" on:click={(e) => addDigit(e)} value=".">.</button>
 			</div>
 			<div class="col-1">
-				<button class="btn btn-secondary" on:click={() => getResult()}>=</button>
+				<button class="btn btn-secondary" on:click={() => getResult()} value="=">=</button>
 			</div>
 			<div class="col-2">
-				<button class="btn btn-secondary" on:click={() => addDigit()}>*</button>
+				<button class="btn btn-secondary" on:click={(e) => addDigit(e)} value="*">*</button>
 			</div>
 		</div>
 	</div>
